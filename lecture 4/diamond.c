c8c6f5e0fae1c9b88c2fba8fd1592aa7abdba07d
xuzheng465 <xuzheng465@gmail.com>
2020-11-14 13:05:25 +0000

    finish lecture code dimond

--- lecture 4/diamond.c
+++ lecture 4/diamond.c
@@ -20,16 +20,53 @@
 #include <string.h>
 #include <error.h>
 
-void diamond(char *str) {
-    // TODO
+void printstr(char *str, int start, int end)
+{
+    if (start == 0)
+    {
+        for (int i = start; i <= end; i++)
+        {
+            printf("%c", str[i]);
+        }
+        return;
+    }
+    for (int j = 0; j < start; j++)
+    {
+        printf(" ");
+    }
+    for (int l = start; l < end; l++)
+    {
+        printf("%c", str[l]);
+    }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc == 1) {
+void diamond(char *str)
+{
+    // TODO
+    int size = strlen(str);
+    // printf("The size of str %d\n", size);
+    for (int j = 0; j < size; j++)
+    {
+        printstr(str, 0, j);
+        printf("\n");
+    }
+    for (int j = 1; j < size; j++)
+    {
+        printstr(str, j, size);
+        printf("\n");
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc == 1)
+    {
         error(1, 0, "Usage: ./diamond <TEXT>");
-    } else {
+    }
+    else
+    {
         diamond(argv[1]);
     }
-    
+
     return 0;
 }
